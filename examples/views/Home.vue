afc426afcea27aea1a8df4e7beada70b5d599183
zhangxb02 <zhangxb02@yesly.com.cn>
2019-05-17 16:49:25 +0800

    no message

--- examples/views/Home.vue
+++ examples/views/Home.vue
@@ -1,11 +1,10 @@
 <template>
   <div class="home">
-    <house-img-swipe :data="data"/>
+    <house-img-swipe :data="data" @click="handleClick" @change="handleChange"/>
   </div>
 </template>
 
 <script>
-
 export default {
   name: 'home',
   data () {
@@ -16,9 +15,20 @@ export default {
         { img: 'http://dev-public-bucket.oss-cn-shanghai.aliyuncs.com/webapp/temp/WechatIMG3.jpeg' }
       ]
     }
+  },
+  methods:{
+    //点击当前显示时触发
+    handleClick(activeItem){
+      console.log(activeItem)
+    },
+    //当前显示的图片切换时触发
+    handleChange(activeItem){
+      console.log(activeItem)
+    },
   }
 }
 </script>
+
 <style lang="scss">
 *{
   box-sizing: border-box;
